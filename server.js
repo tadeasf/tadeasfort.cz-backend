cec371e248cefec8b160f9b21def13c830432f6c
tadeasf <taddy.fort@gmail.com>
2023-07-23 16:00:42 +0000

    fallback pre-cookies

--- server.js
+++ server.js
@@ -1,161 +1,91 @@
 /** @format */
 
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const rateLimit = require("express-rate-limit");
-const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https:/tadeasfort.cz",
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
+app.use(cors());
+app.use(express.json()); // parse JSON bodies
 
-const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 20,
-  keyGenerator: function (req) {
-    return req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  },
-});
-
-app.use("/vote", limiter);
-
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 
-db.on("error", (error) => console.error("Database connection error:", error));
+db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
+// Define schema for your documents
 const blogSchema = new mongoose.Schema({
   id: String,
-  slug: String,
+  slug: String, // new field
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
-  identifiers: { type: Array, default: [] },
+  voters: { type: Array, default: [] },
 });
 
+// Define the Model
 const Blog = mongoose.model("Blog", blogSchema, "likes");
 
-const voteSchema = new mongoose.Schema({
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: "Blog" },
-  identifier: { type: String, required: true },
-  voteType: { type: String, enum: ["like", "dislike"], required: true },
-});
-
-const Vote = mongoose.model("Vote", voteSchema);
-
-app.get("/api/vote/:slug", async (req, res) => {
+// Handle get votes requests
+app.get("/vote/:slug", async (req, res) => {
   try {
-    let identifier = req.ip || req.cookies.userIdentifier;
     let post = await Blog.findOne({ slug: req.params.slug });
-    let userVoted = false;
 
     if (!post) {
-      return res.json({ likes: 0, dislikes: 0, userVoted });
+      return res.json({ likes: 0, dislikes: 0 });
     }
 
-    if (post.identifiers.includes(identifier)) {
-      userVoted = true;
-    }
-
-    res.json({ ...post.toObject(), userVoted });
-    console.log("Vote data retrieved"); // Log successful vote data retrieval
+    res.json(post);
   } catch (err) {
     res.status(500).json({ message: err.message });
-    console.error("Vote data retrieval failed:", err.message); // Log error for vote data retrieval
   }
 });
 
-app.get("/api/vote/:slug/userVoted", async (req, res) => {
+// Handle like/dislike requests
+app.post("/vote/:slug/:vote", async (req, res) => {
   try {
-    let identifier = req.ip || req.cookies.userIdentifier;
+    const ip = req.ip;
 
-    if (!identifier) {
-      return res.json({ userVoted: false });
-    }
-
-    let post = await Blog.findOne({
-      slug: req.params.slug,
-      identifiers: identifier,
-    });
-
-    res.json({ userVoted: !!post });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-    console.error("Failed to check if user voted:", err.message);
-  }
-});
-
-app.post("/api/vote/:slug/:vote", async (req, res) => {
-  try {
-    let identifier = req.ip || req.cookies.userIdentifier;
-
-    if (!identifier) {
-      identifier = Math.random().toString(36).substr(2, 9);
-      console.log("No IP, generated identifier: " + identifier);
-    }
-
+    // First find the post
     let post = await Blog.findOne({ slug: req.params.slug });
 
-    // Check if post exists
-    if (!post) {
-      return res.status(404).json({ message: "Post not found" });
-      console.error("Post not found"); // Log error for post not found
-    }
-
-    let vote = await Vote.findOne({ postId: post._id, identifier });
-
-    if (vote) {
+    if (post && post.voters.includes(ip)) {
       return res.status(403).json({ message: "You've already voted" });
-      console.warn("Multiple voting attempt"); // Log warning for multiple voting attempt
     }
+
+    let update;
 
     if (req.params.vote === "like") {
-      post = await Blog.findOneAndUpdate(
-        { slug: req.params.slug },
-        { $inc: { likes: 1 } },
-        { new: true } // This option returns the updated document
-      );
+      update = { $inc: { likes: 1 }, $push: { voters: ip } };
     } else if (req.params.vote === "dislike") {
-      post = await Blog.findOneAndUpdate(
-        { slug: req.params.slug },
-        { $inc: { dislikes: 1 } },
-        { new: true } // This option returns the updated document
-      );
+      update = { $inc: { dislikes: 1 }, $push: { voters: ip } };
     } else {
       return res.status(400).json({ message: "Invalid vote" });
-      console.error("Invalid vote"); // Log error for invalid vote
     }
 
-    vote = new Vote({
-      postId: post._id,
-      identifier,
-      voteType: req.params.vote,
-    });
-    await vote.save();
-    console.log("New vote"); // Log successful new vote
+    // If post was not found create a new one, otherwise update the existing one
+    if (!post) {
+      post = new Blog({
+        slug: req.params.slug,
+        likes: req.params.vote === "like" ? 1 : 0,
+        dislikes: req.params.vote === "dislike" ? 1 : 0,
+        voters: [ip],
+      });
+      await post.save();
+    } else {
+      post = await Blog.findOneAndUpdate({ slug: req.params.slug }, update, {
+        new: true,
+      });
+    }
 
-    res.cookie("userIdentifier", identifier, {
-      maxAge: 900000,
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
-    });
-
-    res.json({ likes: post.likes, dislikes: post.dislikes });
-    console.log("Vote success"); // Log successful vote response
+    res.json(post);
   } catch (err) {
     res.status(500).json({ message: err.message });
-    console.error("Vote failed:", err.message); // Log error for vote handling failure
   }
 });
 
